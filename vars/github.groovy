1f2b3b922a4682b0ed81e8a9c857a3eab7e8278a
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-06-10 13:50:16 +0700

    feat(github): add optional name, body param to createRelease

--- vars/github.groovy
+++ vars/github.groovy
@@ -1,9 +1,18 @@
-def createRelease(credentials, owner, repo, tag) {
+import groovy.json.JsonOutput
+
+def createRelease(credentials, owner, repo, tag_name, name = null, body = null) {
+  def request = ['tag_name': tag_name]
+  if (name) {
+    request['name'] = name
+  }
+  if (body) {
+    request['body'] = body
+  }
   httpRequest(
     url : "https://api.github.com/repos/${owner}/${repo}/releases",
-    httpMode: "POST",
-    requestBody: """{"tag_name":"${tag}"}""",
-    contentType: "APPLICATION_JSON",
+    httpMode: 'POST',
+    requestBody: JsonOutput.toJson(request),
+    contentType: 'APPLICATION_JSON',
     authentication: credentials,
     validResponseCodes: '200:201'
   )
