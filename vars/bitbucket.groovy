88edd2b0463116ac9fddbef4e306fb6e087fcd25
Dhanar Adi Dewandaru <86091871+dhanarab@users.noreply.github.com>
2022-12-21 12:37:28 +0700

    feat(bitbucket): set build status return response

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,5 +1,5 @@
 def setBuildStatus(credentials, commitHref, state, key, name, description = "", url = "") {  // state: SUCCESSFUL, INPROGRESS, FAILED
-  httpRequest(
+  return httpRequest(
     url : "${commitHref}/statuses/build",
     httpMode: "POST",
     requestBody: """{"state":"${state}", "key":"${key}", "name":"${name}", "description":"${description}", "url":"${url}"}""",
